b804232f4fd01a9dfe8502d7ed86df9717f866e7
Kallirroi <kallirroi.retzepi@gmail.com>
2018-05-12 18:38:16 -0400

    added distort code but too much...

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,32 @@ import Helmet from 'react-helmet'
 import "../styles/index.css";
 
 class BlogIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: {}
+    }
+    this.distort=this.distort.bind(this);
+    this.stopDistort=this.stopDistort.bind(this);
+  }
+
+  componentWillMount() {
+    this.distort(this.props);
+    this.stopDistort(this.props);
+  }
+
+  distort(event) {
+    this.setState({
+      style: {transform: `skew(${event.pageX/10}deg)`}
+    });
+  }  
+
+  stopDistort(event) {
+    this.setState({
+      style: {transform: `skew(0)`}
+    });
+  }
+
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
@@ -14,11 +40,11 @@ class BlogIndex extends React.Component {
         <div className="index">
           <Helmet title={siteTitle} />
           {posts.map(({ node }) => {
-            const title = get(node, 'frontmatter.title') || node.fields.slug
-            const tag = get(node, 'frontmatter.tag') || node.fields.slug
+            const title = get(node, 'frontmatter.title') || node.fields.slug;
+            const tag = get(node, 'frontmatter.tag') || node.fields.slug;
             return (
-              <div className="postThumbnail" key={node.fields.slug}>
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+              <div className="postThumbnail" onMouseEnter={this.distort} onMouseLeave={this.stopDistort} style={this.state.style} key={node.fields.slug}>
+                <Link to={node.fields.slug}>
                   {title}
                 </Link>
                 <div className="tagThumbnail">{tag}</div>
