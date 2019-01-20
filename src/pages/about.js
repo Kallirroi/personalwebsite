45acc5ed9b6e12193da3906c15fbd60166f7a670
Kallirroi <kallirroi.retzepi@gmail.com>
2019-01-20 14:58:30 -0500

    added a bit about confs

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import "../styles/about.css";
 
 export default () => (
   <div className="aboutText">
-    <p>Kalli Retzepi is a graduate student at the Media Lab. She uses technology, design and images in order to explore the politics of digital interfaces, the narrative of the user and to imagine new metaphors for the Web.</p> 
+    <p>Kalli Retzepi is a graduate student at the Media Lab. She uses technology, design and images in order to explore the politics of digital interfaces, the narrative of the user and to imagine new metaphors for the Web. She has recently presented her work in conferences like <a href="http://www.gredits.org/interfacepolitics/en/"> Interface Politics</a> and the <a href="https://decentralizedweb.net/">Decentralized Web Summit</a>. </p>
     <p>Apart from the Internet, she is interested in coffee, reading, running and film photography. Some of her photographic work has been featured by collectives like If You Leave (2017 Showcase), C41 magazine and VelvetEyes.</p>
     <p className="aboutLinks">
     	<a href="https://www.are.na/kalli-retzepi/index" target="_blank">are.na</a> <br/> <a href="https://twitter.com/kallirroi" target="_blank">twitter</a> <br/> <a href="https://www.instagram.com/kallirroi/" target="_blank">instagram</a> <br/>  <a href="mailto:kalli@mit.edu" target="_blank">email</a>
