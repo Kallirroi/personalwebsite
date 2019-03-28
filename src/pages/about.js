f1d079e681880574443e61708fbd9544e3303183
Kallirroi <kallirroi.retzepi@gmail.com>
2019-03-28 12:06:37 -0400

    changes to About section

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,11 +4,31 @@ import "../styles/about.css";
 
 export default () => (
   <div className="aboutText">
-    <p>Kalli Retzepi is a graduate student at the Media Lab. She uses technology, design and images in order to explore the politics of digital interfaces, the narrative of the user and to imagine new metaphors for the Web. She has recently presented her work in conferences like <a href="http://www.gredits.org/interfacepolitics/en/"> Interface Politics</a> and the <a href="https://decentralizedweb.net/">Decentralized Web Summit</a>. </p>
-    <p>Apart from the Internet, she is interested in coffee, reading, running and film photography. Some of her photographic work has been featured by collectives like If You Leave (2017 Showcase), C41 magazine and VelvetEyes.</p>
-    <p>CV available upon request.</p>
+    <br/>
+    <p>Kalli Retzepi is a graduate student at the Media Lab. She uses technology, design and images in order to explore the politics of digital interfaces, the narrative of the user and to imagine new metaphors for the Web. </p>
+
+    <h4>workshops / talks / abstracts</h4>
+    <p>“Questions of utopia in the era of decentralisation” (co-author) - XIV. <a href="https://www.bauhaus-kolloquium.de/">Internationales Bauhaus-Kolloquium</a>, 2019</p>
+    <p>“You the users”, talk at <a href="http://www.gredits.org/interfacepolitics/en/">Interface Politics</a> Conference, 2018</p>
+    <p>“Internet as a city”, workshop at <a href="https://decentralizedweb.net/">Decentralized Web Summit</a>, 2018</p>
+
+    <h4>online features </h4>
+    <p><a href="https://walkerart.org/magazine/soundboard-reenvisioning-internet-gary-zhexi-zhang">clickhere</a> - Reinvisioning the Internet, article by Gary Zhexi Zhang</p>
+    <p><a href="http://www.internetactu.net/2019/01/14/retro-design-de-lattention-cest-complique/">the quiet spaces</a> - Rétro-design de l’attention, article by Hubert Guillaud</p>
+    <p><a href="https://news.sky.com/story/in-the-future-youll-mine-cryptocurrency-by-dancing-11374514"> proof of dance </a> - on Sky News</p>
+    <p><a href="https://www.instagram.com/ifyouleavestagram/">If You Leave</a> film photography collective, 2017 Showcase</p>
+    <p><a href="https://www.artupon.com/kalli-retzepi/">ArtUpon</a> feature</p>
+    <p><a href="https://velveteyes.net/features/kalli-retzepi/">VelvetEyes</a> feature</p>
+    <p><a href="http://www.c41magazine.it/kalli-retzepi-goes-find-light/">C41</a> Magazine feature</p>
+
+
+    <h4>contact</h4>
     <p className="aboutLinks">
-    	<a href="https://www.are.na/kalli-retzepi/index" target="_blank">are.na</a> <br/> <a href="https://twitter.com/kallirroi" target="_blank">twitter</a> <br/> <a href="https://www.instagram.com/kallirroi/" target="_blank">instagram</a> <br/>  <a href="mailto:kalli@mit.edu" target="_blank">email</a>
+      <a href="mailto:kalli@mit.edu" target="_blank">email</a>
+      <a href="https://www.are.na/kalli-retzepi/index" target="_blank">are.na</a>
+      <a href="https://twitter.com/kallirroi" target="_blank">twitter</a>
+      <a href="https://www.instagram.com/kallirroi/" target="_blank">instagram</a>
     </p>
+
   </div>
 );
