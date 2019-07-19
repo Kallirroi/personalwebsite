1386f1e25825b640dd0d66ea30318bd2c7d3067b
Kallirroi <kallirroi.retzepi@gmail.com>
2019-07-19 20:14:03 +0200

    cleaning up until build breaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,6 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
-        <Bio />
         <Link to="/about/">About</Link> 
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -26,47 +26,9 @@ class BlogPostTemplate extends React.Component {
         >
           {post.frontmatter.title}
         </h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio />
 
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+
       </Layout>
     )
   }
