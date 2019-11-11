bf7e2e32736435e69aeb738c2711427ad5cd6062
Kallirroi <kallirroi.retzepi@gmail.com>
2019-11-11 08:59:00 -0500

    added selected projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,9 +27,22 @@ class BlogIndex extends React.Component {
 
           <p style={{
             marginBottom: rhythm(1),
-          }}>I use code, images and words to explore the politics of digital interfaces, the narrative of the user and to imagine new metaphors for the Web. I have degrees in engineering and in neuroscience, and most recently from the MIT Media Lab.</p>
+          }}>I use code, images and words to explore the politics of digital interfaces, the narrative of the user and to imagine new metaphors for the Web. I have degrees in engineering and in neuroscience, and I am a recent alum of the MIT Media Lab. I know my path sounds meandering, but what can I say, I guess I enjoy learning.</p>
 
-          <p>I am currently working as a freelancer out of Brooklyn. Feel free to reach out about work inquiries.</p>
+          <p>I am currently working as a web design and dev freelancer out of Brooklyn, NY. Feel free to <a href="mailto:kalli.retzepi@gmail.com" target="_blank">get in touch</a> about work inquiries.</p>
+
+          <h4 style={{
+            marginTop: rhythm(2),
+          }}>selected web-based work</h4>
+          <ul style={{
+                  listStyleType: 'none',
+                  lineHeight: '22px'
+                }}>
+            <li><a href="https://kalli-retzepi.com/PROPS/" target="_blank">PROPS</a></li>
+            <li><a href="https://kalli-retzepi.com/roadtoschool/" target="_blank">The road to school</a></li>
+            <li><a href="https://kalli-retzepi.com/samtous/" target="_blank">samtous.wtf</a></li>
+            <li><a href="https://www.interactivethings.com/work/explora/" target="_blank">Explora</a>, as part of <a href="https://www.interactivethings.com/" target="_blank">Interactive Things</a></li>
+          </ul>
 
           <h4 style={{
             marginTop: rhythm(2),
